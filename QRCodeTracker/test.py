29d0e1d6f3eb036d9c85be4b2f4a2d0427d95ffc
ConnorWorrell <chemicalcoal1@gmail.com>
2020-11-14 03:39:56 -0700

    Added rotation to the test

--- QRCodeTracker/test.py
+++ QRCodeTracker/test.py
@@ -11,12 +11,29 @@ cap = cv2.VideoCapture('QrCodeTestVideo.avi')
 
 Y_Values = np.array(np.concatenate((np.linspace(1.54,6,60),np.linspace(6,6,40),np.linspace(6,1,30),np.linspace(1,1,30),np.linspace(1,6,30),np.linspace(6,.363,40),np.linspace(.363,.363,40))))
 X_Values = np.array(np.concatenate((np.linspace(0,0,60),np.linspace(0,-1,40),np.linspace(-1,-1,30),np.linspace(-1,1,30),np.linspace(1,1,30),np.linspace(1,0,10),np.linspace(0,0,30),np.linspace(0,-.444,20),np.linspace(-.444,.469,20))))
-
-print(X_Values)
+R_Values = np.array(np.concatenate((np.linspace(0,0,100),np.linspace(0,38.8,30),np.linspace(38.8,-39,30),np.linspace(-39,0,40),np.linspace(0,0,70))))
 
 frame = 0
 
 deviation = []
+
+img = cv2.imread("QRCodeBoard12.png")
+
+Corners,WidthOfScan,HeightOfScan,xPosOfLeftSideOfScan,VanishingPoint,img = QRCodeFromTutorial.FindCorners(img)
+if Corners != None:
+    x,y,rot = QRCodeFromTutorial.GetDistance(Corners,WidthOfScan,HeightOfScan, xPosOfLeftSideOfScan,VanishingPoint,img,RefRenceDistance=1.619,RefrenceHeight=872.82,h_fov = 40)
+
+    # print(x,y,rot)
+    if x != None:
+        dev = [x-X_Values[frame],y-Y_Values[frame],rot-R_Values[frame]]
+        print(dev)
+        deviation.append(dev)
+        QRCodeFromTutorial.displayPosition(x, y, rot)
+    else:
+        print("Error")
+
+cv2.imshow("Result", img)
+cv2.waitKey(0)
 
 while(cap.isOpened()):
 
@@ -26,15 +43,16 @@ while(cap.isOpened()):
 
     Corners,WidthOfScan,HeightOfScan,xPosOfLeftSideOfScan,VanishingPoint,img = QRCodeFromTutorial.FindCorners(img)
     if Corners != None:
-        x,y,rot = QRCodeFromTutorial.GetDistance(Corners,WidthOfScan,HeightOfScan, xPosOfLeftSideOfScan,VanishingPoint,img,RefRenceDistance=1.619,RefrenceHeight=872.82)
+        x,y,rot = QRCodeFromTutorial.GetDistance(Corners,WidthOfScan,HeightOfScan, xPosOfLeftSideOfScan,VanishingPoint,img,RefRenceDistance=1.619,RefrenceHeight=872.82,h_fov = 40)
 
         # print(x,y,rot)
         if x != None:
-            deviation.append([x-X_Values[frame],y-Y_Values[frame]])
+            dev = [abs(x-X_Values[frame]),abs(y-Y_Values[frame]),abs(rot-R_Values[frame])]
+            print(dev)
+            deviation.append(dev)
             QRCodeFromTutorial.displayPosition(x, y, rot)
         else:
             print("Error")
-        print(x,y,X_Values[frame],Y_Values[frame])
 
     cv2.imshow("Result", img)
     cv2.waitKey(5)
@@ -42,6 +60,9 @@ while(cap.isOpened()):
 
 xdev = [x[0] for x in deviation]
 ydev = [y[1] for y in deviation]
+rdev = [y[2] for y in deviation]
 
-print("Xdev: {} meters".format(statistics.stdev(xdev)))
-print("Ydev: {} meters".format(statistics.stdev(ydev)))
+print("Xdev: {}, Xmax: {} meters".format(statistics.stdev(xdev),max(xdev)))
+print("Ydev: {}, Ymax: {} meters".format(statistics.stdev(ydev),max(ydev)))
+print("Rdev: {}, Rmax: {} degrees".format(statistics.stdev(rdev),max(rdev)))
+
