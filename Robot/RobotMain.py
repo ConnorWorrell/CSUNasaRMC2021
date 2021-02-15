ca7a291085af606c032794ff1560ba031acd3bb9
ConnorWorrell <chemicalcoal1@gmail.com>
2021-02-14 17:56:30 -0700

    Camera code appears to be functional

--- Robot/RobotMain.py
+++ Robot/RobotMain.py
@@ -4,8 +4,13 @@ from StateMachine import StateMachine as SM1
 
 # Initilize Sensors + Motors
 Cameras = Cam1()
-Cameras.AddCamera(1920,1080)
-
+Cameras.AddCamera(640,480,.25,0,90,1,500,120)
+# Cameras.AddCamera(1920,1080,0,0,1,500,120)
+# Cameras.AnalizeCameras()
+while(1):
+    Cameras.AnalizeCameras()
+    # print(Cameras.RobotLocationInfo)
+    Cameras.displayPosition()
 # Initilize other cameras and sensors here
 
 # InitilizeCommunication()
