dbe4b9dba2725c2ca47415fd16c625daf6995e32
ConnorWorrell <chemicalcoal1@gmail.com>
2021-02-28 00:16:09 -0800

    Slave tasks no longer initilize cameras on startup

--- Robot/RobotMain.py
+++ Robot/RobotMain.py
@@ -7,38 +7,56 @@
 
 # ListenForData()
 
-def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
-    dim = None
-    (h, w) = image.shape[:2]
 
-    # if both the width and height are None, then return the
-    # original image
-    if width is None and height is None:
-        return image
-
-    # check to see if the width is None
-    if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
-
-    # otherwise, the height is None
-    else:
-        # calculate the ratio of the width and construct the
-        # dimensions
-        r = width / float(w)
-        dim = (width, int(h * r))
-
-    # resize the image
-    resized = cv2.resize(image, dim, interpolation=inter)
-
-    # return the resized image
-    return resized
 
 if __name__ == '__main__':
+    from Cameras import Cameras as Cam1
+    import CommunicationRobot
+    from multiprocessing import Process
+    from StateMachine import StateMachine as SM1
+    import globals
+    from cv2 import *
+    import commands
+
+
+    def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+        # initialize the dimensions of the image to be resized and
+        # grab the image size
+        dim = None
+        (h, w) = image.shape[:2]
+
+        # if both the width and height are None, then return the
+        # original image
+        if width is None and height is None:
+            return image
+
+        # check to see if the width is None
+        if width is None:
+            # calculate the ratio of the height and construct the
+            # dimensions
+            r = height / float(h)
+            dim = (int(w * r), height)
+
+        # otherwise, the height is None
+        else:
+            # calculate the ratio of the width and construct the
+            # dimensions
+            r = width / float(w)
+            dim = (width, int(h * r))
+
+        # resize the image
+        resized = cv2.resize(image, dim, interpolation=inter)
+
+        # return the resized image
+        return resized
+
+    # Initilize Sensors + Motors
+    Cameras = Cam1()
+    # Cameras.AddCamera(640,480,.25,0,90,1,500,120)
+    Cameras.AddCamera(1920, 1080, 0, 0, 0, 1, 500, 120)
+    Cameras.AddCamera(1920, 1080, 0, 0, 0, 1, 500, 120)
+    print(Cameras.CameraStorage)
+
     globals.initilizeGlobals()
     sharedData = globals.sharedData
     # CommunicationRobot.InitilizeCommunication()
