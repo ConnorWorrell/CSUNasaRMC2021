34fbe6bbc037035695bf9bf2a5277861975c072e
ConnorWorrell <chemicalcoal1@gmail.com>
2021-03-07 13:14:26 -0800

    Motor control setup

--- Robot/motor.py
+++ Robot/motor.py
@@ -30,161 +30,155 @@ def map_syren(speed):
         speed = int(speed)
         return speed
 
+def motorSpeedControl(sharedMotorSpeedData):
+    class motors:
+        def __init__(self):
+            self.status = "Initializing"
+            logging.info("Initializing motor drivers")
+            self.RRSpeed = 0
+            self.RLSpeed = 0
+            # Arduinos must be flashed with Standard Firmata
+            try:
+                self.ard0 = PyMata3(arduino_wait=2, com_port='COM6')  # Lattepanda onboard arduino
+                # self.ard1 = PyMata3(arduino_wait=2, com_port='/dev/ttyACM1')  # Additional arduino micro
+            except:
+                logging.error("Motor fail")
+                self.status = "FAIL"
+                return
 
-class motors:
-    def __init__(self):
-        self.status = "Initializing"
-        logging.info("Initializing motor drivers")
+            # FL
+            self.FL = 3
+            self.ard0.servo_config(self.FL, 1000, 2000)
 
-        # Arduinos must be flashed with Standard Firmata
-        try:
-            self.ard0 = PyMata3(arduino_wait=2, com_port='COM4')  # Lattepanda onboard arduino
-            #self.ard1 = PyMata3(arduino_wait=2, com_port='/dev/ttyACM1')  # Additional arduino micro
-        except:
-            logging.error("Motor fail")
-            self.status = "FAIL"
+            # FR
+            self.FR = 5
+            self.ard0.servo_config(self.FR, 1000, 2000)
+
+            # RL
+            self.RL = 6
+            self.ard0.servo_config(self.RL, 1000, 2000)
+
+            # RR
+            self.RR = 9
+            self.ard0.servo_config(self.RR, 1000, 2000)
+
+            # Auger
+            # self.m4 = 5
+            # self.ard1.servo_config(self.m4, 1000, 2000)
+
+            # Slider
+            # self.m5 = 3
+            # self.ard1.servo_config(self.m5, 1000, 2000)
+
+            # Tilt-mining
+            # self.m6 = 10
+            # self.ard0.servo_config(self.m6, 1000, 2000)
+
+            # Deposit bucket
+            # self.m7 = 11
+            # self.ard0.servo_config(self.m7, 1000, 2000)
+
+            self.stop()
+
+            self.status = "OK"  # TODO: real status check (arduinos present, etc)
+
+        # Wheel FL
+        def SetMotorFL(self, speed):
+            speed = map_sabertooth(speed * -1)
+            self.ard0.analog_write(self.FL, speed)
             return
 
-        # FL
-        self.m0 = 3
-        self.ard0.servo_config(self.m0, 1000, 2000)
+        # Wheel FR
+        def SetMotorFR(self, speed):
+            speed = map_sabertooth(speed * -1)
+            self.ard0.analog_write(self.FR, speed)
+            return
 
-        # FR
-        self.m1 = 5
-        self.ard0.servo_config(self.m1, 1000, 2000)
+        # Wheel RL
+        def SetMotorRL(self, speed):
+            speed = map_sabertooth(speed * -1)
+            self.ard0.analog_write(self.RL, speed)
+            return
 
-        # RL
-        self.m2 = 6
-        self.ard0.servo_config(self.m2, 1000, 2000)
+        # Wheel RR
+        def SetMotorRR(self, speed):
+            speed = map_sabertooth(speed * -1)
+            self.ard0.analog_write(self.RR, speed)
+            return
 
-        # RR
-        self.m3 = 9
-        self.ard0.servo_config(self.m3, 1000, 2000)
+        # # Auger
+        # def aug(self, speed):
+        #     speed = map_syren(speed)
+        #     self.ard1.analog_write(self.m4, speed)
+        #     return
+        #
+        # # Slider
+        # def sld(self, speed):
+        #     speed = map_syren(speed - 0.12)  # Zero speed is slightly off
+        #     self.ard1.analog_write(self.m5, speed)
+        #     return
+        #
+        # # Tilt-mining
+        # def tlt(self, speed):
+        #     speed = map_sabertooth(speed)
+        #     self.ard0.analog_write(self.m6, speed)
+        #     return
+        #
+        # # Deposit bucket
+        # def bkt(self, speed):
+        #     speed = map_sabertooth(speed)
+        #     self.ard0.analog_write(self.m7, speed)
+        #     return
 
-        # Auger
-        #self.m4 = 5
-        #self.ard1.servo_config(self.m4, 1000, 2000)
+        def stop(self, smooth=False):
+            # TODO smooth stop
+            logging.info("Stopping motors")
+            self.SetMotorRR(0)
+            self.SetMotorRL(0)
+            return self.status
 
-        # Slider
-        #self.m5 = 3
-        #self.ard1.servo_config(self.m5, 1000, 2000)
+        # Map L,R tank drive to wheel number
+        def tank(self, left, right):
+            self.wheel(0, left)
+            self.wheel(1, right)
+            self.wheel(2, left)
+            self.wheel(3, right)
 
-        # Tilt-mining
-        #self.m6 = 10
-        #self.ard0.servo_config(self.m6, 1000, 2000)
+        # Map X,Y from joystick to L,R tank drive
+        def direction(self, speed, dir):
+            # Math from http://home.kendra.com/mauser/joystick.html
+            v = (1 - abs(dir)) * speed + speed
+            w = (1 - abs(speed)) * dir + dir
+            l = (v + w) / 2
+            r = (v - w) / 2
+            self.tank(l, r)
 
-        # Deposit bucket
-        #self.m7 = 11
-        #self.ard0.servo_config(self.m7, 1000, 2000)
+        def heartbeat(self):
+            # Provide beat for watchdog to enable drivers
+            while self.status == "OK":
+                # TODO
+                pass
 
-        self.stop()
+        def updateMotors(self):
+            motors.SetMotorRR(self.RRSpeed)
+            motors.SetMotorRL(self.RLSpeed)
 
-        self.status = "OK"  # TODO: real status check (arduinos present, etc)
-
-    # Wheel FL
-    def FL(self, speed):
-        speed = map_sabertooth(speed * -1)
-        self.ard0.analog_write(self.m0, speed)
-        return
-
-    # Wheel FR
-    def FR(self, speed):
-        speed = map_sabertooth(speed * -1)
-        self.ard0.analog_write(self.m1, speed)
-        return
-
-    # Wheel RL
-    def RL(self, speed):
-        speed = map_sabertooth(speed * -1)
-        self.ard0.analog_write(self.m2, speed)
-        return
-
-    # Wheel RR
-    def RR(self, speed):
-        speed = map_sabertooth(speed * -1)
-        self.ard0.analog_write(self.m3, speed)
-        return
-
-    # Auger
-    def aug(self, speed):
-        speed = map_syren(speed)
-        self.ard1.analog_write(self.m4, speed)
-        return
-
-    # Slider
-    def sld(self, speed):
-        speed = map_syren(speed - 0.12)  # Zero speed is slightly off
-        self.ard1.analog_write(self.m5, speed)
-        return
-
-    # Tilt-mining
-    def tlt(self, speed):
-        speed = map_sabertooth(speed)
-        self.ard0.analog_write(self.m6, speed)
-        return
-
-    # Deposit bucket
-    def bkt(self, speed):
-        speed = map_sabertooth(speed)
-        self.ard0.analog_write(self.m7, speed)
-        return
-
-    def stop(self, smooth=False):
-        # TODO smooth stop
-        logging.info("Stopping motors")
-        self.FL(0)
-        self.FR(0)
-        self.RL(0)
-        self.RR(0)
-        #self.aug(0)
-        #self.sld(0)
-        #self.tlt(0)
-        #self.bkt(0)
-        return self.status
-
-    # Map wheel number to motors
-    def wheel(self, num, speed):
-        if num == 0:
-            self.FL(speed)
-        elif num == 1:
-            self.FR(speed)
-        elif num == 2:
-            self.RL(speed)
-        elif num == 3:
-            self.RR(speed)
-
-    # Map L,R tank drive to wheel number
-    def tank(self, left, right):
-        self.wheel(0, left)
-        self.wheel(1, right)
-        self.wheel(2, left)
-        self.wheel(3, right)
-
-    # Map X,Y from joystick to L,R tank drive
-    def direction(self, speed, dir):
-        # Math from http://home.kendra.com/mauser/joystick.html
-        v = (1 - abs(dir)) * speed + speed
-        w = (1 - abs(speed)) * dir + dir
-        l = (v + w) / 2
-        r = (v - w) / 2
-        self.tank(l, r)
-
-    def heartbeat(self):
-        # Provide beat for watchdog to enable drivers
-        while self.status == "OK":
-            # TODO
-            pass
-
-
-# Testing
-if __name__ == "__main__":
     motors = motors()
-    motors.stop(False)
-    for i in range(-1000, 1000, 1):
-        #motors.wheel(0, i / 1000)
-        #motors.wheel(1, i / 1000)
-        #motors.wheel(2, i / 1000)
-        motors.wheel(3, i / 1000)
-        print(i / 1000)
-        time.sleep(0.01)
-    motors.stop()
+    while (1):
+        if "RRSpeed" in sharedMotorSpeedData.keys():
+            motors.RRSpeed = sharedMotorSpeedData["RRSpeed"]
+        if "RLSpeed" in sharedMotorSpeedData.keys():
+            motors.RLSpeed = sharedMotorSpeedData["RLSpeed"]
+        motors.updateMotors()
+
+if __name__ == "__main__":
+    motorSpeedControl({"RRSpeed": 0.0, "RLSpeed": 0.0})
+    # motors = motors()
+    # motors.stop(False)
+    # for i in range(-1000, 1000, 1):
+    #     motors.RRSpeed = i / 1000
+    #     motors.RLSpeed = i / 1000
+    #     motors.updateMotors()
+    #     print(i / 1000)
+    #     time.sleep(0.01)
+    # motors.stop()
